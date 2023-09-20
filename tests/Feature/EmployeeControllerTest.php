faa55ca6511f272b421adc93c9505b09a4700455
Pascual Eburi <pascualeburi@gmail.com>
2023-09-20 23:55:01 +0200

    Add tests for [update] method

--- tests/Feature/EmployeeControllerTest.php
+++ tests/Feature/EmployeeControllerTest.php
@@ -33,6 +33,7 @@ class EmployeeControllerTest extends TestCase
             'avatar' => $file,
         ];
 
+        
 
         $response = $this->json('POST', '/store', $data);
         $response->assertStatus(200)
@@ -95,7 +96,7 @@ class EmployeeControllerTest extends TestCase
     /**
      * @test
      */
-    public function test_it_returns_error_for_invalid_employee_id()
+    public function test_it_returns_error_for_invalid_employee_id_when_fetching_employee_data()
     {
         // Proporciona un ID de empleado que no existe en la base de datos
         $invalidEmployeeId = 999;
@@ -111,6 +112,141 @@ class EmployeeControllerTest extends TestCase
             $json->has('message')
         );
     }
+
+
+    /**===============================================
+     * Update method tests
+     =====================================================*/
+    public function test_it_can_update_an_employee_with_new_avatar() {
+        $employee = Employee::factory()->create();
+
+        Storage::fake('local');
+
+        $file = UploadedFile::fake()->image('new_avatar.jpg');
+        $file_name = $file->hashName();
+
+
+        $newData = [
+            'emp_id' => $employee->id,
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'email' => $this->faker->email,
+            'phone' => $this->faker->phoneNumber,
+            'job_position' => $this->faker->jobTitle,
+            'date_hired' => $this->faker->date,
+            'avatar' => $file,
+        ];
+
+        $response = $this->json('POST', '/update', $newData);
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('employees', [
+            'id' => $employee->id,
+            'email' => $newData['email'],
+            'phone' => $newData['phone'],
+            'avatar' => 'avatars/' . $file_name
+        ]);
+
+        Storage::disk('local')->assertExists('avatars/' .$newData['avatar']->hashName());
+        Storage::disk('public')->assertMissing($employee->avatar);
+    }
+
+    /**
+     * @test
+     */
+    public function test_it_handles_validation_errors()
+    {
+        // Crea un empleado de ejemplo en la base de datos
+        $employee = factory(Employee::class)->create();
+
+        // Genera datos simulados para la solicitud de actualización con errores de validación
+        $invalidData = [
+            'emp_id' => $employee->id,
+            'first_name' => '', // Campo requerido en las reglas de validación
+            // Agrega más campos inválidos aquí según tus reglas de validación
+        ];
+
+        // Realiza una solicitud POST JSON al endpoint de actualización de empleado con datos inválidos
+        $response = $this->json('POST', '/update', $invalidData);
+
+        // Verifica que la respuesta tenga un código de estado 200 (aunque los datos sean inválidos)
+        $response->assertStatus(200);
+
+        // Verifica que la respuesta contenga los errores de validación en formato JSON
+        $response->assertJsonValidationErrors([
+            'first_name', // Nombre del campo que falló la validación
+            // Agrega más campos aquí según tus reglas de validación
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function test_it_handles_employee_not_found()
+    {
+        // Proporciona un ID de empleado que no existe en la base de datos
+        $nonExistingEmployeeId = 999;
+
+        // Genera datos simulados para la solicitud de actualización
+        $newData = [
+            'emp_id' => $nonExistingEmployeeId,
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'email' => $this->faker->email,
+            'phone' => $this->faker->phoneNumber,
+            'job_position' => $this->faker->jobTitle,
+            'date_hired' => $this->faker->date,
+            'avatar' => UploadedFile::fake()->image('new_avatar.jpg'),
+        ];
+
+        // Realiza una solicitud POST JSON al endpoint de actualización de empleado con ID no válido
+        $response = $this->json('POST', '/update', $newData);
+
+        // Verifica que la respuesta tenga un código de estado 404 (not found)
+        $response->assertStatus(404);
+
+        // Verifica que la respuesta contenga un mensaje de error en formato JSON
+        $response->assertJson([
+            'message' => 'Employee not found',
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function test_it_handles_internal_server_error(){
+        // Simula un error interno al actualizar el empleado (puedes ajustar esta lógica según tu necesidad)
+        $this->mock(Storage::class, function ($mock) {
+            $mock->shouldReceive('storeAs')->andReturn(false);
+        });
+
+        // Crea un empleado de ejemplo en la base de datos
+        $employee = factory(Employee::class)->create();
+
+        // Genera datos simulados para la solicitud de actualización
+        $newData = [
+            'emp_id' => $employee->id,
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'email' => $this->faker->email,
+            'phone' => $this->faker->phoneNumber,
+            'job_position' => $this->faker->jobTitle,
+            'date_hired' => $this->faker->date,
+            'avatar' => UploadedFile::fake()->image('new_avatar.jpg'),
+        ];
+
+        // Realiza una solicitud POST JSON al endpoint de actualización de empleado
+        $response = $this->json('POST', '/update', $newData);
+
+        // Verifica que la respuesta tenga un código de estado 500 (internal server error)
+        $response->assertStatus(500);
+
+        // Verifica que la respuesta contenga un mensaje de error en formato JSON
+        $response->assertJson([
+            'message' => 'Error Message Here', // Puedes ajustar este mensaje según tu lógica de error
+        ]);
+    }
+
+
 
         
     /**--------------------------------------------------------
@@ -133,7 +269,7 @@ class EmployeeControllerTest extends TestCase
     /**
      * @test
      */
-    public function it_returns_error_for_non_existing_employee(){
+    public function it_returns_error_for_non_existing_employee_when_deleting_employee(){
 
         $nonExistingEmployeeId = 999;
         $this->json('DELETE', "/delete", ['id'=>$nonExistingEmployeeId])
@@ -146,7 +282,7 @@ class EmployeeControllerTest extends TestCase
     /**
      * @test
      */
-    public function it_returns_error_for_internal_server_error(){
+    public function it_returns_error_for_internal_server_error_when_deleting_employee(){
 
         $employee = Employee::factory()->create();
         $this->json('DELETE', "/delete" , ['id' => $employee])
