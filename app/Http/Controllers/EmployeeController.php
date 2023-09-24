0669c2f0bf9b3ced2fe14bcede69b8440ed81f65
Pascual Eburi <pascualeburi@gmail.com>
2023-09-24 02:25:38 +0200

    Added types for parameters and returns types for all methods

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,16 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Employee;
 use DateTime;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class EmployeeController extends Controller{
     /**
      * Display a listing of the resource.
-     */
+     **/
 
-    private $rules = [
+    private array $rules = [
         'first_name' => 'required|max:20',
         'last_name' => 'required|max:40',
         'email' => 'required|max:255',
@@ -21,15 +23,17 @@ class EmployeeController extends Controller{
         'job_position' => 'required|max:50',
         'date_hired' => 'required|date|before_or_equal:today',
         'avatar' => 'required|file|mimes:jpg,gif,png'
-        
+
     ];
 
 
     /**
      * Handle employee list ajax request
+     * @throws \Exception
+     * @returns string
      */
 
-    public function index(){
+    public function index():string{
         // get all employees
         $result = array('data' => array());
         $employees = Employee::all();
@@ -47,17 +51,16 @@ class EmployeeController extends Controller{
             $index++;
             # storage/images/'
             $photo = '<img src="storage/avatars/'. $employee->avatar.'" width="60" height="60" class="img-thumbnail rounded-circle" style="aspect-ratio:1/1;object-fit: cover;">';
-            
+
             // buttons for actions
             // data-bs-toggle="modal" data-bs-target="#editEmployeeModal"
-            $buttons = '                
-            <td>
-            <a href="#" data-employee-id="' . $employee->id . '" class="text-success mx-1 editIcon" ><i class="bi-pencil-square h4"></i></a>
+            $buttons = "<td>
+            <a href=\"#\" data-employee-id=\"" . $employee->id . "\" class=\"text-success mx-1 editIcon\" ><i class=\"bi-pencil-square h4\"></i></a>
 
-            <a href="#" data-employee-id="' . $employee->id . '" class="text-danger mx-1 deleteIcon"><i class="bi-trash h4"></i></a>
-          </td>';
+            <a href=\"#\" data-employee-id=\"" . $employee->id . "\" class=\"text-danger mx-1 deleteIcon\"><i class=\"bi-trash h4\"></i></a>
+          </td>";
 
-          
+
           $hired_date = new DateTime($employee->date_hired);
           $seniority = $today->diff($hired_date)->days;
 
@@ -74,29 +77,29 @@ class EmployeeController extends Controller{
           );
         }
 
-
-
         return json_encode($result);
-        ;
+
     }
-
-
 
 
     /**
      * Handle a create employee ajax request
+     * @param Request $request
+     * @return string|JsonResponse
+     * @throws ValidationException
      */
-    public function store(Request $request){
+    public function store(Request $request): string |JsonResponse
+    {
         // validator
 
         $validator = Validator::make($request->all(), $this->rules);
 
-        // stop validating as soon as we found a validation error 
+        // stop validating as soon as we found a validation error
         if ($validator->stopOnFirstFailure()->fails()) {
             return json_encode($validator->validated()['errors']);
-            
+
         }
-        
+
         $file_name = $request->file('avatar')->hashName();
 
         # $file->storeAs('public/images', $fileName);
@@ -106,9 +109,9 @@ class EmployeeController extends Controller{
 
         //$request->file('avatar')->store('avatars');
 		$employeeData = ['first_name' => $request->first_name, 'last_name' => $request->last_name, 'email' => $request->email, 'phone' => $request->phone, 'job_position' => $request->job_position,'date_hired' => $request->date_hired, 'avatar' => $file_name];
-		
+
         try {
-            
+
             Employee::create($employeeData);
             return response()->json([
                 'message' => 'Employee added successfully',
@@ -121,42 +124,49 @@ class EmployeeController extends Controller{
         }
 
 
-		
+
     }
 
 
 
     /**
      * Handle get single employee ajax request.
+     * @param Request $request;
+     * @returns JsonResponse;
      */
-    public function show(Request $request){
-        // find employeee
+    public function show(Request $request): JsonResponse
+    {
+        // find employee
         try {
             $id = $request->id;
             $employee = Employee::find($id);
             if ($employee){
                 return response()->json($employee);
             }
-            
+
             return response()->json(['message' => "Employee not found"],404);
-            
+
         } catch (\Throwable $th) {
             //throw $th;
             return response()->json([
                 'message' => $th->getMessage()
             ], 500);
         }
-      
+
 
     }
 
     /**
      * Handle update employee ajax request.
+     * @param Request $request;
+     * @return JsonResponse|string;
+     *@throws ValidationException
      */
-    public function update(Request $request){
-        //delete avatar
+    public function update(Request $request): JsonResponse|string
+    {
+
         $file_name = '';
-        // find employeee
+        // find employee
         try {
             $employee = Employee::find($request->emp_id);
             if(!$employee){
@@ -180,15 +190,15 @@ class EmployeeController extends Controller{
         // validate employee data
         $validator = Validator::make($request->all(), $this->rules);
 
-        // stop validating as soon as we found a validation error 
+        // stop validating as soon as we found a validation error
         if ($validator->stopOnFirstFailure()->fails()) {
             return json_encode($validator->validated()['errors']);
         }
 
-        // so on, everithing ok, 
+        // so on, everything ok,
 
         if ( $request->hasFile('avatar') ){
-            
+
 /*             $file_name = $request->file('avatar')->store('avatars');
             $file_name = $request->photo->store('images', 'public'); */
 
@@ -209,9 +219,9 @@ class EmployeeController extends Controller{
         $employee_data = ['first_name' => $request->first_name, 'last_name' => $request->last_name, 'email' => $request->email, 'phone' => $request->phone, 'job_position' => $request->job_position,'date_hired' => $request->date_hired, 'avatar' => $file_name
         ];
 
-        // udate employee data
+        // update employee data
         try {
-            
+
             $employee->update($employee_data);
             return response()->json([
                 'message' => 'Employee updated successfully successfully'
@@ -224,26 +234,35 @@ class EmployeeController extends Controller{
         }
 
 
-        
+
     }
 
     /**
      * Handle delete employee ajax request.
+     * @param Request $request;
+     * @returns JsonResponse
      */
-    public function destroy(Request $request){
+    public function destroy(Request $request):JsonResponse{
         //
         try {
             $employee = Employee::find($request->id);
-            if(!$employee || $employee === null){
+            if(!$employee){
                 return response()->json([
                     'message' => 'Employee Not Found',
                 ], 404);
             }
 
-            # 'public/images/' . 
+            # 'public/images/' .
             if (Storage::delete( 'public/avatars/' . $employee->avatar)){
                 Employee::destroy($request->id);
+                return response()->json([
+                    'message' => 'Employee Deleted Successfully',
+                ], 200);
             }
+
+            return response()->json([
+                'message' => "Unable to delete employee avatar",
+            ], 500);
 
         } catch (\Throwable $th) {
             //throw $th;
@@ -251,6 +270,6 @@ class EmployeeController extends Controller{
                 'message' => $th->getMessage()
             ], 500);
         }
-        
+
     }
 }
